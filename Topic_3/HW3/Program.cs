9cddd9b83497d6caa315c3dbbb03a69bee676685
slav41k <115183875+slav41k@users.noreply.github.com>
2023-08-02 23:49:18 +0300

    hw3
    hw3

---
+++ Topic_3/HW3/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace HW3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Task 1
+            
+            Console.Write("Enter text: ");
+            string str = Console.ReadLine();
+            int count = 0;
+            foreach (char symbol in str)
+            {
+                if (symbol == 'a' || symbol == 'o' || symbol == 'e' || symbol == 'i')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"The counts of characters ‘a’, ’o’, ’i’, ’e’  in this text : {count}");
+
+            
+            //Task 2
+            Console.Write("Enter the year: ");
+            int year = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter the number of month: ");
+            int month = Int32.Parse(Console.ReadLine());
+            int daysInMonth = DateTime.DaysInMonth(year, month);
+            Console.WriteLine(daysInMonth);
+            
+            
+            //Task 3
+
+            int[] numbers = new int[10];
+            bool firstFiveNumbersArePositive = true;
+            int sum = 0;
+            int product = 1;
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("Your numbers: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]}; ");
+            }
+            Console.WriteLine();
+            
+            for (int i = 0; i < numbers.Length / 2; i++)
+            {
+                if (numbers[i] <= 0)
+                {
+                    firstFiveNumbersArePositive = false;
+                    break;
+                }
+            }
+
+            if (firstFiveNumbersArePositive)
+            {
+                for (int i = 0; i < numbers.Length / 2; i++)
+                {
+                    sum += numbers[i];
+                }
+            }
+            else
+            {
+                for (int i = numbers.Length / 2; i < numbers.Length; i++)
+                {
+                    product *= numbers[i];
+                }
+            }
+
+            Console.WriteLine((firstFiveNumbersArePositive)
+                ? $"Sum of first 5 positive numbers: {sum}"
+                : $"Product of last 5 numbers: {product}");
+
+        }
+    }
+}
---
+++ Topic_3/Task 3/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        //Task 1
+        
+        Console.Write("Enter 2 numbers [a;b]:\na = ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("b = ");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Numbers from {a} to {b} which divided by 3 without remainder:");
+        for (int i = Math.Min(a, b); i <= Math.Max(a, b); i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        
+        
+        //Task 2
+        
+        Console.WriteLine("Enter some text and receive each second character:");
+        string text = Console.ReadLine();
+        for (int i = 0; i < text.Length; i += 2)
+            Console.Write(text[i]);
+        Console.WriteLine();
+        
+        
+        //Task 3
+        
+        Console.Write("Enter the name of the drink (coffee, tea, juice, water):");
+        string input = Console.ReadLine();
+        switch (input.ToLower())
+        {
+            case "tea" :
+                Console.WriteLine($"{30:C}");
+                break;
+            case    "water" :
+                Console.WriteLine($"{20:C}");
+                break;
+            case "coffee" :
+            case "juice" :
+                Console.WriteLine($"{45:C}");
+                break;
+            default:
+                Console.WriteLine("We don't have such drinks, sorry bro");
+                break;
+        }
+        
+        
+        //Task 4
+        
+        Console.WriteLine("Array : { 1, 5, 7, 43, -6 }");
+        Console.ReadKey();
+        int[] nums = { 1, 5, 7, 43, -6 };
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+            sum += (nums[i] >= 0) ? nums[i] : 0;
+        int average = sum / (nums.Length - 1);
+        Console.WriteLine($"Sum: {sum}\nAvarage: {average}");
+        
+        
+        //Task 5
+        
+        Console.Write("Enter random year: ");
+        int year = Int32.Parse(Console.ReadLine());
+        Console.WriteLine((year % 4 == 0) ? $"{year} is leap" : $"{year} is not leap year");
+        
+        
+        //Task 6
+        
+        Console.Write("Enter random number : ");
+        int num = Int32.Parse(Console.ReadLine());
+        int sumOfNum = 0;
+        while (num != 0)
+        {
+            sumOfNum += (num % 10);
+            num /= 10;
+        }
+        Console.WriteLine($"Sum of digits =  {sumOfNum}");
+        
+        
+        //Task 7
+        
+        Console.Write("Enter number: ");
+        int number = Int32.Parse(Console.ReadLine());
+        bool isAllDigitsOdd = true;
+        while (number != 0)
+        {
+            if (number % 2 == 0)
+            {
+                isAllDigitsOdd = false;
+                break;
+            }
+            else
+                isAllDigitsOdd = true;
+
+            number /= 10;
+        }
+        Console.WriteLine(isAllDigitsOdd ? "Number contains only odd digits" : "Number contains even digits");
+        
+    }
+    
+}
